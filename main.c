8006cec80d18e34b0960113eecf62fcdfd3a3a21
NathanGros <nathan.gros6@gmail.com>
2024-10-07 14:39:25 +0200

    added Pixel structure and matrix for the game

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define GRID_HEIGHT 20
+#define GRID_WIDTH 10
+
+
+typedef struct {
+	bool isFull;
+	Color color;
+} Pixel;
 
 
 //Init
@@ -23,6 +31,13 @@ int main() {
 	Init(backgroundColor);
 	int screenWidth = GetScreenWidth();
 	int screenHeight = GetScreenHeight();
+	Pixel mat[GRID_WIDTH][GRID_HEIGHT];
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			mat[i][j].isFull = false;
+			mat[i][j].color = (Color) {0, 0, 0, 255};
+		}
+	}
 
 	while (!WindowShouldClose()) {
 	//Resize
