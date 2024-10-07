17f68e38d0b20247ffd656b4e84bc4dc4d2aba4e
NathanGros <nathan.gros6@gmail.com>
2024-10-07 13:30:28 +0200

    added window support

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <raylib.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+//Init
+
+void Init(Color backgroundColor) {
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1500, 900, "Window");
+	ClearBackground(backgroundColor);
+	SetTargetFPS(60);
+}
+
+
+
+//Main
+
+int main() {
+	//Init
+	Color backgroundColor = (Color) {200, 200, 200, 255};
+	Init(backgroundColor);
+	int screenWidth = GetScreenWidth();
+	int screenHeight = GetScreenHeight();
+
+	while (!WindowShouldClose()) {
+	//Resize
+		if (IsWindowResized()) {
+			screenWidth = GetScreenWidth();
+			screenHeight = GetScreenHeight();
+		}
+	
+	//Drawing
+		BeginDrawing();
+			ClearBackground(backgroundColor);
+		EndDrawing();
+	}
+	CloseWindow();
+
+	//De-init
+	return 0;
+}
