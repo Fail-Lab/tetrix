ab530efafb4e95d48c43ef4c89bed83d3d1e957d
NathanGros <nathan.gros6@gmail.com>
2024-10-13 23:57:58 +0200

    added grid display to see what's happening

--- main.c
+++ main.c
@@ -8,8 +8,22 @@
 
 typedef struct {
 	bool isFull;
+	bool isStatic;
 	Color color;
 } Pixel;
+
+
+
+//Draw
+
+void DrawBoard(Pixel mat[GRID_WIDTH][GRID_HEIGHT], int pixelSize) {
+	DrawRectangle(100, 0, GRID_WIDTH * pixelSize, GRID_HEIGHT * pixelSize, BLACK);
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			DrawRectangle(100 + i * pixelSize, j * pixelSize, pixelSize - 1, pixelSize - 1, mat[i][j].color);
+		}
+	}
+}
 
 
 //Init
@@ -27,17 +41,23 @@ void Init(Color backgroundColor) {
 
 int main() {
 	//Init
-	Color backgroundColor = (Color) {0, 0, 0, 0};
+	Color backgroundColor = (Color) {10, 10, 10, 255};
 	Init(backgroundColor);
 	int screenWidth = GetScreenWidth();
 	int screenHeight = GetScreenHeight();
+	int pixelSize = screenHeight / GRID_HEIGHT;
 	Pixel mat[GRID_WIDTH][GRID_HEIGHT];
 	for (int i = 0; i < GRID_WIDTH; i++) {
 		for (int j = 0; j < GRID_HEIGHT; j++) {
 			mat[i][j].isFull = false;
-			mat[i][j].color = (Color) {0, 0, 0, 255};
+			mat[i][j].isStatic = false;
+			mat[i][j].color = (Color) {20, 20, 20, 255};
 		}
 	}
+	mat[2][3].color = MAGENTA;
+	mat[3][2].color = MAGENTA;
+	mat[3][3].color = MAGENTA;
+	mat[3][4].color = MAGENTA;
 
 	while (!WindowShouldClose()) {
 	//Resize
@@ -49,6 +69,7 @@ int main() {
 	//Drawing
 		BeginDrawing();
 			ClearBackground(backgroundColor);
+			DrawBoard(mat, pixelSize);
 		EndDrawing();
 	}
 	CloseWindow();
