7b0a99d393f187736b37f07bd484c6236c984883
NathanGros <nathan.gros6@gmail.com>
2024-10-14 09:12:23 +0200

    added time support and falling blocks

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <raylib.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
 
 #define GRID_HEIGHT 20
 #define GRID_WIDTH 10
@@ -26,7 +25,7 @@ void DrawBoard(Pixel mat[GRID_WIDTH][GRID_HEIGHT], int pixelSize) {
 }
 
 
-//Init
+//Functions
 
 void Init(Color backgroundColor) {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
@@ -35,12 +34,50 @@ void Init(Color backgroundColor) {
 	SetTargetFPS(60);
 }
 
+void fallBlock(Pixel mat[GRID_WIDTH][GRID_HEIGHT]) {
+	bool isStatic = false;
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = GRID_HEIGHT - 1; j >= 0; j--) {
+			if (!mat[i][j].isStatic) {
+				if (j == GRID_HEIGHT - 1 || mat[i][j+1].isFull)
+					isStatic = true;
+			}
+		}
+	}
+	if (isStatic) {
+		for (int i = 0; i < GRID_WIDTH; i++) {
+			for (int j = GRID_HEIGHT - 1; j >= 0; j--) {
+				if (!mat[i][j].isStatic) {
+					mat[i][j].isStatic = true;
+					mat[i][j].isFull = true;
+				}
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < GRID_WIDTH; i++) {
+			for (int j = GRID_HEIGHT - 1; j >= 0; j--) {
+				if (!mat[i][j].isStatic) {
+					mat[i][j+1].isStatic = false;
+					mat[i][j+1].color = mat[i][j].color;
+					mat[i][j].isStatic = true;
+					mat[i][j].color = (Color) {20, 20, 20, 255};
+				}
+			}
+		}
+	}
+}
+
 
 
 //Main
 
 int main() {
 	//Init
+	float time = 0.;
+	float timeBetweenFrames = 0.5;
+	int frameCount = 0;
+	int score = 0;
 	Color backgroundColor = (Color) {10, 10, 10, 255};
 	Init(backgroundColor);
 	int screenWidth = GetScreenWidth();
@@ -50,14 +87,18 @@ int main() {
 	for (int i = 0; i < GRID_WIDTH; i++) {
 		for (int j = 0; j < GRID_HEIGHT; j++) {
 			mat[i][j].isFull = false;
-			mat[i][j].isStatic = false;
+			mat[i][j].isStatic = true;
 			mat[i][j].color = (Color) {20, 20, 20, 255};
 		}
 	}
 	mat[2][3].color = MAGENTA;
+	mat[2][3].isStatic = false;
 	mat[3][2].color = MAGENTA;
+	mat[3][2].isStatic = false;
 	mat[3][3].color = MAGENTA;
+	mat[3][3].isStatic = false;
 	mat[3][4].color = MAGENTA;
+	mat[3][4].isStatic = false;
 
 	while (!WindowShouldClose()) {
 	//Resize
@@ -66,6 +107,16 @@ int main() {
 			screenHeight = GetScreenHeight();
 		}
 	
+	//Variable update
+		time += GetFrameTime();
+	
+	//advance time
+		if ((float) (frameCount + 1) * timeBetweenFrames < time) {
+			printf("frame %d\n", frameCount);
+			fallBlock(mat);
+			frameCount++;
+		}
+
 	//Drawing
 		BeginDrawing();
 			ClearBackground(backgroundColor);
